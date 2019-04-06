f5ff35fc2cef1c97de48aeea817672871c93507d
bri sun <highlevelstructures@gmail.com>
2019-04-06 19:01:16 +0100

    initial cm

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+<<<<<<< HEAD
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+=======
+<<<<<<< HEAD
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('name');
+            $table->string('ucountry');
+            $table->string('utype');
+            $table->string('email')->unique();
+            $table->string('username')->unique();
+=======
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+>>>>>>> 0e984abf5beb29d03b5b5167e7cf4e37c0f627f9
+>>>>>>> 20ea42017d45b958f4be186ecdc3e5440e876a66
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
